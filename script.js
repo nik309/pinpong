d88237c624686d1d6f0598b3f345a0b49d8a4a5c
nik309 <potoskuev1812@otlook.com>
2024-11-28 21:36:05 +0500

    2изм

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const grid = 15;
 const paddleHeight = grid * 5;
 const maxPaddleY = map.height - grid - paddleHeight;
 
-let ballSpeed = 10;
+let ballSpeed = 4;
 let paddleSpeed = 5;
 
 const leftPaddle = {
@@ -47,6 +47,75 @@ function renderMap() {
 function clearMap() {
     canvas.clearRect(0, 0, map.width, map.height);
 }
+function resetGame() {
+    if ((ball.x < 0 || ball.x > map.width) && !ball.isResetted) {
+        ball.isResetted = true;
+        setTimeout(() => {
+            ball.x = map.width / 2;
+            ball.y = map.height / 2;
+            ball.isResetted = false;
+        }, 1000);
+    }
+}
+
+function collideWallsWithPaddle(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
+    }
+}
+
+function collideWallsWithPaddles() {
+    collideWallsWithPaddle(leftPaddle);
+    collideWallsWithPaddle(rightPaddle);
+}
+function collideWallsWithBall() {
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy = -ball.dy;
+    }
+    else if (ball.y > map.height - grid) {
+        ball.y = map.height - grid;
+        ball.dy = -ball.dy;
+    }
+}
+
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+function aiControl() {
+    let direction = 0 ;
+
+    if (ball.y < rightPaddle.y) {
+        direction = -1;
+    }
+    else if (ball.y > rightPaddle.y + paddleHeight) {
+        direction = 1;
+    }
+
+    rightPaddle.y += paddleSpeed * direction;
+}
+
+function collidePaddlesWithBall() {
+    if (isCollides(ball, rightPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = rightPaddle.x - ball.width;
+    }
+    else if (isCollides(ball, leftPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = leftPaddle.x + leftPaddle.width;
+    }
+}
+
 
 function loop() {
     clearMap();
@@ -55,9 +124,19 @@ function loop() {
     renderRightPaddle();
     movePaddles();
 
+   aiControl();
+
+     collideWallsWithPaddle(leftPaddle);
+  
     renderBall();
     moveBall();
 
+    collideWallsWithBall();
+    collidePaddlesWithBall();
+
+
+
+    resetGame();
     renderMap();
     requestAnimationFrame(loop);
 }
